c81a2ac4d6579b9400cac9334f2048cf763f2ab7
Manojkumar Ramu <manoramu39@gmail.com>
2025-10-08 15:57:43 +0530

    Added start script and Render-compatible port

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,8 @@ const payslipRoutes = require("./routes/payslip");
 
 
 const app = express();
-const PORT = 5000;
+//const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const MONGO_URI = 'mongodb://localhost:27017/NEWDemo_Db';
 
 // ---------------- MIDDLEWARE ---------------- //
