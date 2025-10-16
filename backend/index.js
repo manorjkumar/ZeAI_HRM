0ddaff5d2813f83040a4cf83defc087f5858d2e4
Manojkumar Ramu <manoramu39@gmail.com>
2025-10-16 10:44:16 +0530

    url change

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
 
 app.use(cors({
   origin: [
-    "https://ubiquitous-elf-83cae9.netlify.app", // 🔁 Replace with your actual Netlify domain
+    "https://zeaisofthrm.netlify.app/", // 🔁 Replace with your actual Netlify domain
     //"http://localhost:3000" // for local testing (optional)
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
